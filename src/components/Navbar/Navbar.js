b053c08a80289c8fa4aaef3d1d64384c28bcb4d0
Benjamin Reichwein <ben.basque@icloud.com>
2020-10-08 13:31:23 -0400

    CSS Fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,7 +18,7 @@ class Navbar extends Component {
     render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">BOOST <i className="fas fa-heartbeat"/></h1>
+                <h1 className="navbar-logo">BOOST <i className="fas fa-bolt"/></h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
